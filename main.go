e978c8d9964f2c14c7181876a88025ec9a338782
Engin Yüce <enginy88@gmail.com>
2023-12-08 01:11:32 +0300

    Version update.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var appSett *app.AppSettStruct
 func main() {
 
 	start := time.Now()
-	app.LogAlways.Println("HELLO MSG: Welcome to PAN-USOM-XML2EDL v2.5 by EY!")
+	app.LogAlways.Println("HELLO MSG: Welcome to PAN-USOM-XML2EDL v2.6 by EY!")
 
 	appFlag = app.GetAppFlag()
 	appSett = app.GetAppSett()
